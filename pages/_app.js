86d95ac5f9179f28aeee7c6102e1bf13c7d6b5bf
Kokosnootje <judacat@hotmail.com>
2023-06-18 13:37:57 +0200

    Test 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import 'nextra-theme-blog/style.css'
-import Head from 'next/head'
 
 import '../styles/main.css'
 
@@ -21,7 +20,6 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
-      <Component {...pageProps} />
     </>
   )
 }
